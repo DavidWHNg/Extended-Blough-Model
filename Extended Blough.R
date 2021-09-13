1f4dde38fbf001f07d4f4ee5236ec6a4023940be
David Ng <z5019453@ad.unsw.edu.au>
2021-09-13 19:51:10 +1000

    13092021
    - Fixed up all bugs and errors in codes (as of 13/09/2021)
    - Set initial values to simulate Experiment 1A
    - Wrote up a README to help instruct others how to use.

--- Extended Blough.R
+++ Extended Blough.R
@@ -2,6 +2,7 @@ rm(list=ls())
 options(scipen=999)
 library(dplyr)
 library(ggplot2)
+library(reshape2)
 
 #### define parameters ####
 #Inputs 
@@ -9,48 +10,45 @@ beta_plus <- 0.2
 beta_minus <- 0.25
 lamb_plus <- 1
 lamb_minus <- 0
-sd <- 10 #activation function sigma
+sd <- 1
 
 # Experimental design
-name <- 'Jess exemplars_sd20' #name (for plotting)
-stim_num_d1 <- 100 # how many stimuli on dimension 1? -> sets dimension size/stimuli distance
-stim_inc_d1 <- 0.25 # how many elements between stimuli? -> sets smoothness of plots
-stim_num_d2 <- 100 # how many stimuli on dimension 2?
-stim_inc_d2 <- 0.25 # how many elements between stimuli?
-dim_lims <- 5 #set boundaries of stimulus dimension, increase to prevent activation functions cutting off for stimuli at extremes
+name <- 'Exp1A' 
+stim_num_d1 <- 11 
+stim_inc_d1 <- 0.25
+stim_num_d2 <- 11 
+stim_inc_d2 <- 0.25 
+dim_lims <- 5
 
-g1_name <- "positive only"
-g1_training_stimuli_names <- c("CS1+","CS2+","CS3+","CS4+")
-g1_training_trialtypes <- c(1,1,1,1) # 1 = reinforce, 0 = non-reinforce
-g1_d1_training_stimuli_elements <- c(50,75,50,25)
-g1_d2_training_stimuli_elements <- c(75,50,25,50)
-g1_N <- 0 #how many participants?
+g1_name <- "Single Positive"
+g1_training_stimuli_names <- c("CS+")
+g1_training_trialtypes <- c(1) 
+g1_d1_training_stimuli_elements <- c(6)
+g1_d2_training_stimuli_elements <- c(1)
+g1_N <- 46 
 
-g2_name <- "positive-negative"
-g2_training_stimuli_names <- c("CS1+","CS2+","CS3+","CS4+","CS1-","CS2-","CS3-","CS4-")
-g2_training_trialtypes <- c(1,1,1,1,0,0,0,0) 
-g2_d1_training_stimuli_elements <- c(50,75,50,25,50,85,50,15)
-g2_d2_training_stimuli_elements <- c(75,50,25,50,85,50,15,50)
-g2_N <- 1
+g2_name <- "Double Negative"
+g2_training_stimuli_names <- c("CS1-","CS+","CS2-")
+g2_training_trialtypes <- c(0,1,0) 
+g2_d1_training_stimuli_elements <- c(4,6,8)
+g2_d2_training_stimuli_elements <- c(1,1,1)
+g2_N <- 50 
 
 #test stimuli
-test_stimuli_names <- c("CS1+","CS2+","CS3+","CS4+",
-                        "CS1-","CS2-","CS3-","CS4-",
-                        "T1","T2","T3","T4","T5","T6","T7","T8")
-d1_test_stimuli_elements <- c(50,75,50,25,
-                              50,85,50,15,
-                              50,60,50,40,50,95,50,5)
-d2_test_stimuli_elements <- c(75,50,25,50,
-                              85,50,15,50,
-                              60,50,40,50,95,50,5,50)
+test_stimuli_names <- c("S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","S11")
+d1_test_stimuli_elements <- c(1:11)
+d2_test_stimuli_elements <- c(rep(1,11))
 
-stimreps_per_block <- 2
+#experiment design
+stimreps_per_block <- 2 
 num_blocks <- 6
-partial_reinf <- FALSE # if TRUE, randomly turns CS+ trials into CS- 
-partial_reinf_rate <- 0.75
+
+#saving data
+savetraining <-  TRUE
+savetest <-  TRUE
 
 #Misc.
-digits = 20 # rounding for elemental activation function, change to set how much spreading activation each stimuli has/use if elemental activation function for stimuli at extremes getting cut off too much
+digits <-  20
 
 
 ####### MODEL #######
@@ -106,10 +104,10 @@ g2_training_stimuli$trialtype <- as.numeric(g2_training_stimuli$trialtype)
 g2_training_stimuli$d1_e <- as.numeric(as.character(g2_training_stimuli$d1_e))
 g2_training_stimuli$d2_e <- as.numeric(as.character(g2_training_stimuli$d2_e))
 
-test_stimuli <- data.frame(matrix(c(paste0("S",1:length(d1_test_stimuli_elements)),
+test_stimuli <- data.frame(matrix(c(test_stimuli_names,
                        d1_test_stimuli_elements,
                        d2_test_stimuli_elements),
-                       nrow=length(d1_test_stimuli_elements),
+                       nrow=length(test_stimuli_names),
                        ncol=3))
 
 colnames(test_stimuli) <- c("stimname","d1_e","d2_e")
@@ -117,7 +115,7 @@ test_stimuli$d1_e <- as.numeric(as.character(test_stimuli$d1_e))
 test_stimuli$d2_e <- as.numeric(as.character(test_stimuli$d2_e))
 
 ##experiment design
-trainingdata <- list()
+trainingdata <- data.frame()
 testdata <- data.frame()
 
 ##loop training for every participant
@@ -133,24 +131,17 @@ for (i in 1:(g1_N+g2_N)) {
   }
   
   #set random trial orders + outcomes (1/0 shock/no-shock)
-  
-  training_trials_order <- sample(c(rep(as.character(training_stimuli$stimname)
-                                        ,num_blocks*stimreps_per_block)))
-  
-  #set partial reinforcement
-  if (partial_reinf == TRUE){
-    trial_outcome_order[sample(which(trial_outcome_order==1),(1-partial_reinf_rate)*stimreps_per_block*num_blocks,replace=FALSE)] <- 0
-    
-  }
+  training_trials_order <- data.frame(stimname = sample(c(rep(as.character(training_stimuli$stimname)
+                                                              ,num_blocks*stimreps_per_block))))
   
   #set up training data frames
-  d1_training <- data.frame(matrix(data = 0, nrow = length(training_trials_order), ncol = length(d1_elems)))
-  colnames(d1_training) <- paste0("e",c(1:length(d1_elems)))
-  rownames(d1_training) <- paste0("trial",c(1:length(training_trials_order)))
+  d1_training <- data.frame(matrix(data = 0, nrow = length(training_trials_order$stimname), ncol = length(d1_elems)))
+  colnames(d1_training) <- paste0("d1_e",c(1:length(d1_elems)))
+  rownames(d1_training) <- paste0("trial",c(1:length(training_trials_order$stimname)))
   
-  d2_training <- data.frame(matrix(data = 0, nrow = length(training_trials_order), ncol = length(d2_elems)))
-  colnames(d2_training) <- paste0("e",c(1:length(d2_elems)))
-  rownames(d2_training) <- paste0("trial",c(1:length(training_trials_order)))
+  d2_training <- data.frame(matrix(data = 0, nrow = length(training_trials_order$stimname), ncol = length(d2_elems)))
+  colnames(d2_training) <- paste0("d2_e",c(1:length(d2_elems)))
+  rownames(d2_training) <- paste0("trial",c(1:length(training_trials_order$stimname)))
   
   ## Simulations
   # function to calculate total associative strength of a stimulus compound on a trial
@@ -163,51 +154,62 @@ for (i in 1:(g1_N+g2_N)) {
   # training
   #trial 1
   #reinforcement
-  if (training_stimuli$trialtype[training_stimuli$stimname == training_trials_order[1]] == 1){
-    d1_training[1,] <-  elem_activation(training_stimuli$d1_e[training_stimuli$stimname == training_trials_order[1]],1)*beta_plus*(lamb_plus - 0)
-    d2_training[1,] <-  elem_activation(training_stimuli$d2_e[training_stimuli$stimname == training_trials_order[1]],2)*beta_plus*(lamb_plus - 0)
+  if (training_stimuli$trialtype[training_stimuli$stimname == training_trials_order$stimname[1]] == 1){
+    d1_training[1,] <-  elem_activation(training_stimuli$d1_e[training_stimuli$stimname == training_trials_order$stimname[1]],1)*beta_plus*(lamb_plus - 0)
+    d2_training[1,] <-  elem_activation(training_stimuli$d2_e[training_stimuli$stimname == training_trials_order$stimname[1]],2)*beta_plus*(lamb_plus - 0)
   }
   
   #non-reinforcement
-  if (training_stimuli$trialtype[training_stimuli$stimname == training_trials_order[1]] == 0){
-    d1_training[1,] <-  elem_activation(training_stimuli$d1_e[training_stimuli$stimname == training_trials_order[1]],1)*beta_minus*(lamb_minus - 0)
-    d2_training[1,] <-  elem_activation(training_stimuli$d2_e[training_stimuli$stimname == training_trials_order[1]],2)*beta_minus*(lamb_minus - 0)
+  if (training_stimuli$trialtype[training_stimuli$stimname == training_trials_order$stimname[1]] == 0){
+    d1_training[1,] <-  elem_activation(training_stimuli$d1_e[training_stimuli$stimname == training_trials_order$stimname[1]],1)*beta_minus*(lamb_minus - 0)
+    d2_training[1,] <-  elem_activation(training_stimuli$d2_e[training_stimuli$stimname == training_trials_order$stimname[1]],2)*beta_minus*(lamb_minus - 0)
   }
   
   #rest of the trials
-  for (j in 2:length(training_trials_order)) {
+  for (j in 2:length(training_trials_order$stimname)) {
     
     #calculate stimulus V for trial
     trial_V <- stimulus_v(
-      training_stimuli$d1_e[training_stimuli$stimname == training_trials_order[j]],
-      training_stimuli$d2_e[training_stimuli$stimname == training_trials_order[j]],
+      training_stimuli$d1_e[training_stimuli$stimname == training_trials_order$stimname[j]],
+      training_stimuli$d2_e[training_stimuli$stimname == training_trials_order$stimname[j]],
       j-1)
     
     #if reinforcement trial
-    if (training_stimuli$trialtype[training_stimuli$stimname == training_trials_order[j]] == 1){
-      d1_training[j,] <-  d1_training[j-1,] + elem_activation(training_stimuli$d1_e[training_stimuli$stimname == training_trials_order[j]],1)*beta_plus*(lamb_plus - trial_V)
-      d2_training[j,] <-  d2_training[j-1,] + elem_activation(training_stimuli$d2_e[training_stimuli$stimname == training_trials_order[j]],2)*beta_plus*(lamb_plus - trial_V)
+    if (training_stimuli$trialtype[training_stimuli$stimname == training_trials_order$stimname[j]] == 1){
+      d1_training[j,] <-  d1_training[j-1,] + elem_activation(training_stimuli$d1_e[training_stimuli$stimname == training_trials_order$stimname[j]],1)*beta_plus*(lamb_plus - trial_V)
+      d2_training[j,] <-  d2_training[j-1,] + elem_activation(training_stimuli$d2_e[training_stimuli$stimname == training_trials_order$stimname[j]],2)*beta_plus*(lamb_plus - trial_V)
     }
     
     #if non-reinforcement trial
-    if (training_stimuli$trialtype[training_stimuli$stimname == training_trials_order[j]] == 0){
-      d1_training[j,] <-  d1_training[j-1,] + elem_activation(training_stimuli$d1_e[training_stimuli$stimname == training_trials_order[j]],1)*beta_minus*(lamb_minus - trial_V)
-      d2_training[j,] <-  d2_training[j-1,] + elem_activation(training_stimuli$d2_e[training_stimuli$stimname == training_trials_order[j]],2)*beta_minus*(lamb_minus - trial_V)
+    if (training_stimuli$trialtype[training_stimuli$stimname == training_trials_order$stimname[j]] == 0){
+      d1_training[j,] <-  d1_training[j-1,] + elem_activation(training_stimuli$d1_e[training_stimuli$stimname == training_trials_order$stimname[j]],1)*beta_minus*(lamb_minus - trial_V)
+      d2_training[j,] <-  d2_training[j-1,] + elem_activation(training_stimuli$d2_e[training_stimuli$stimname == training_trials_order$stimname[j]],2)*beta_minus*(lamb_minus - trial_V)
     }
   }
   
   #save training data
-  participant_trainingdata <- list(d1_training,d2_training)
-  trainingdata[i] <- participant_trainingdata
+  d1_training$pid <- pid
+  d1_training$group <- group
+  d1_training$trialnum <- 1:length(d1_training$d1_e1)
+  d2_training$pid <- pid
+  d2_training$group <- group
+  d2_training$trialnum <- 1:length(d2_training$d2_e1)
+  participant_trainingdata <- merge(d1_training,d2_training, by = c("pid","group","trialnum"))
+  
+  if (pid == 1) {
+    trainingdata <- participant_trainingdata
+  } else {
+    trainingdata <- rbind(trainingdata,participant_trainingdata)
+  } 
 
   # sum associative strengths to simulate test per participant
   # set up data frame
   gen_test <- data.frame(matrix(data = 0, ncol = length(test_stimuli$stimname)))
-  colnames(gen_test) <- test_stimuli$stimname
+    colnames(gen_test) <- test_stimuli$stimname
   for (j in test_stimuli$stimname) {
     gen_test[,j] <- stimulus_v(test_stimuli$d1_e[test_stimuli$stimname == j],
                                test_stimuli$d2_e[test_stimuli$stimname == j],
-                               length(training_trials_order)) #associative strength of each stimulus at the end of training (the weights on the last trial)
+                               length(training_trials_order$stimname)) #associative strength of each stimulus at the end of training (the weights on the last trial)
     gen_test$group <- group
     gen_test$pid <- pid
     gen_test$exp <- name
@@ -218,110 +220,42 @@ for (i in 1:(g1_N+g2_N)) {
 
 
 ##calculate group means
-testdata_means <- data.frame(matrix(data = NA, ncol = length(test_stimuli$stimname)))
-colnames(testdata_means) <- test_stimuli_names
+#change testdata to long format
+testdata <- melt(testdata,id = c("group","pid","exp"),variable.name = "stimname",value.name = "response")
+testdata_means <- aggregate(testdata$response,list(testdata$group,testdata$stimname),mean)
+colnames(testdata_means) <- c("group","stimname","response")
+testdata_means$group <- factor(testdata_means$group)
 
-for (i in 1:length(unique(testdata$group))){
-  tempdata <- data.frame()
-  tempdata <- rbind(tempdata,colMeans(testdata[testdata$group==i,1:length(test_stimuli$stimname)])) 
-  colnames(tempdata) <- test_stimuli_names
-  testdata_means[i,] <- tempdata
+#Save data
+setwd("../Simulated Data")
+if (savetraining == TRUE) {
+  write.table(trainingdata,file=paste0("EB_sim_trainingdata_",name,"_",(Sys.Date()),".txt"),sep="\t",row.names=FALSE)  
+}
+if (savetest == TRUE) {
+  write.table(testdata,file=paste0("EB_sim_testdata_",name,"_",(Sys.Date()),".txt"),sep="\t",row.names=FALSE)  
 }
 
-write.table(testdata,file=paste0("simdata_",name,"_",(Sys.Date()),".txt"),sep="\t",row.names=FALSE)
+#Plotting
+testdata_means
+setwd("../Plots")
 
-
-# plot
-jpeg(filename = paste0("Simulation Gen Test","-",name,"-",g1_name,"-",g2_name,"_",Sys.Date(),".jpeg"),
-     width = 2200, height = 1500, units = "px", pointsize = 12,
+jpeg(filename = paste0(name,"_",format(Sys.time(), "%d-%b-%Y %H.%M.%S"),".jpeg"),
+     width = 4500, height = 1800, units = "px", pointsize = 12,
      quality = 1000,res=200,
      bg = "white")
 
-plot(as.numeric(testdata_means[1,]),
-     type="l", col="hotpink", lwd=3, ylim=c(-1,1),
-     xlab="",ylab="",axes=FALSE,cex.lab=2,ann = FALSE)
-lines(as.numeric(testdata_means[2,]),
-      type="l", col="steelblue", lwd=3)
-
-par(mar=c(5,5,3,2))
-
-points(as.numeric(testdata_means[1,]),
-       pch = 16, col="hotpink",cex=3,lwd=3)
-points(as.numeric(testdata_means[2,]),
-       pch = 15, col="steelblue",cex=3,lwd=3)
-
-legend("topright",c(g1_name,g2_name), 
-       col=c("hotpink","steelblue"),pch=c(16,15), inset=(c(-0.015,-0.03)),pt.cex=3, lwd=3,cex=2, bty="n")
-
-mtext(text = "stimulus",side = 1,line = 3,cex=2)
-mtext(text = "associative strength",side = 2,line = 3,cex=2)
-
-axis(side=1,at=0:stim_num_d1,pos=0,
-     labels=c(0:stim_num_d1),cex.axis=1.5)
-
-axis(side=2,labels=c("-0.5","0.0","0.5","1.0"),at=c(-0.5,0,0.5,1),cex.axis=2)
-
-
-dev.off()
-
-#### graph for Ng et al. (2021) ####
-# plot activation functions
-jpeg(filename = paste0("Simulation activation function","-",name,"-",g1_name,"-",g2_name,"_",Sys.Date(),".jpeg"),
-     width = 2200, height = 1300, units = "px", pointsize = 12,
-     quality = 1000,res=200,
-     bg = "white")
-
-act_functions <- data.frame(CS_plus = elem_activation(6,1),
-                            CS1_minus = elem_activation(8,1), 
-                            CS2_minus = elem_activation(4,1), 
-                            CS_extreme1 = elem_activation(11,1),
-                            CS_extreme2 = elem_activation(1,1))
-
-plot(act_functions$CS_plus,type="p",
-     col=hsv(.489,1,.75),pch=16,
-     cex=1,
-     xlab="",ylab="",axes=FALSE,cex.lab=2)
-points(act_functions$CS1_minus,type="p",
-       col=hsv(.523,1,.75),pch=16,
-       cex=1)
-points(act_functions$CS2_minus,type="p",
-       col=hsv(.447,1,.75),pch=16,
-       cex=1)
-points(act_functions$CS_extreme1,type="p",
-       col=hsv(.583,1,.75),pch=16,
-       cex=1)
-points(act_functions$CS_extreme2,type="p",
-       col=hsv(.396,1,.75),pch=16,
-       cex=1)
-
-
-lines(act_functions$CS_plus,
-      col=hsv(.489,1,.75),
-      lwd=1.5)
-lines(act_functions$CS1_minus,
-      col=hsv(.523,1,.75),
-      lwd=1.5)
-lines(act_functions$CS2_minus,
-      col=hsv(.447,1,.75),pch=16,
-      cex=1.5)
-lines(act_functions$CS_extreme1,
-      col=hsv(.583,1,.75),
-      lwd=1.5)
-lines(act_functions$CS_extreme2,
-      col=hsv(.396,1,.75),
-      lwd=1.5)
-
-par(mar=c(5,5,3,2))
-
-
-legend("topleft",c("GS1","CS1-","CS+","CS2-","GS11"),
-       col=c(hsv(.396,1,.75),hsv(.447,1,.75),hsv(.489,1,.75),hsv(.523,1,.75),hsv(.583,1,.75)),pch=c(16), inset=(.02),pt.cex=1,lwd=2,cex=2,bty="n")
-
-mtext(text = "element",side = 1,line = 3,cex=2)
-mtext(text = "activation",side = 2,line = 3,cex=2)
-
-axis(side=1,pos=0,labels=c("","GS1","CS1-","CS+","CS2-","GS11",""),at=c(0,21,32,41,50,61,81),cex.axis=2)
-axis(side=2,cex.axis=2)
-
+ggplot(testdata_means, 
+       aes(y = response, 
+           x = factor(stimname),
+           group = group,
+           color = group
+       )) + 
+  geom_line(lwd=2) +  
+  geom_point(lwd=7) +
+  ylim (-1,1) +
+  xlab("stimulus") + ylab("associative strength") +
+  theme_gray(base_size = 30) +
+  scale_color_manual(values=c('steelblue','hotpink')) +
+  scale_x_discrete(labels = c(levels(testdata_means$stimname)))
 
 dev.off()
--- README.md
+++ README.md
@@ -1,1 +1,134 @@
-Extended Blough Model
+###Extended Blough Model
+
+This code is to simulate training and test data as predicted by our Extended Blough Model. There are several parameters that can be changed by users to predict generalization for a variety of experimental designs. This code was designed to simulate training and test for 2 groups, but can be used to simulate a single group. As an example, the initial values have been inputed to simulate Experiment 1A. 
+
+Details for each parameter are as follows:
+
+#Inputs 
+beta_plus <- Learning rate for reinforced trials
+beta_minus <- Learning rate for non-reinforced trials
+lamb_plus <- Learning assymptote for reinforced trials
+lamb_minus <- Learning assymptote for non-reinforced trials
+sd <- Standard deviation of Gaussian function for elemental activation
+
+### Experimental design
+name <- name of experiment/design for filenames when saving/plotting
+stim_num_d1 <- sets size of dimension 1 (d1) in stimulus units 
+stim_inc_d1 <- sets how many elemental units between stimuli on d1. Increasing elements smooth out plots, but will take longer to run simulations
+stim_num_d2 <- sets size of dimension 2 (d2) in stimulus units 
+stim_inc_d2 <- sets how many elemental units between stimuli on d2
+dim_lims <- set boundaries of both stimulus dimensions. Increase limits to prevent activation functions cutting off for stimuli at ends of stimulus dimensions
+
+g1_name <- name of group 1 (g1) for filenames when saving/plotting
+g1_training_stimuli_names <- vector of names of g1 stimuli for saving/plotting
+g1_training_trialtypes <- vector of numeric values representing trial types for g1 training stimuli. 1 is reinforcement and 0 is non-reinforcement. 
+g1_d1_training_stimuli_elements <- vector of numeric values representing each training stimulus' most activated element on d1
+g1_d2_training_stimuli_elements <- vector of numeric values representing each training stimulus' most activated element on d2
+g1_N <- sets how many participants to simulate for g1. As this code randomizes training trials, there can be variation between participants that may be smoothed out by repeated simulations. Larger N's may take longer to simulate however. Note: users can set to 0 if they only wish to simulate 1 group. 
+
+g2_name <- name of group 2 (g2) for filenames when saving/plotting
+g2_training_stimuli_names <- vector of names of g2 stimuli for saving/plotting
+g2_training_trialtypes <- vector of numeric values representing trial types for g1 training stimuli.
+g2_d1_training_stimuli_elements <- vector of numeric values representing each training stimulus' most activated element on d1
+g2_d2_training_stimuli_elements <- vector of numeric values representing each training stimulus' most activated element on d2
+g2_N <- sets how many participants to simulate for g2
+
+#test stimuli
+test_stimuli_names <- vector of names of test stimuli for saving/plotting
+d1_test_stimuli_elements <- vector of numeric values representing each test stimulus' most activated element on d1
+d2_test_stimuli_elements <- vector of numeric values representing each test stimulus' most activated element on d1
+
+#experiment design
+stimreps_per_block <- sets how many times a training stimulus is repeated per training trial block
+num_blocks <- sets how many training blocks to simulate
+
+#saving data
+savetraining <-  set to TRUE to save a .txt file of training data
+savetest <-  set to TRUE to save .txt file of test data
+
+#misc.
+digits <-  rounding for elemental activation function. Decrease digits to limit the spreading activation of each stimulus, or use if elemental activation functions substantially cut off for stimuli at ends of dimensions
+
+
+### inputs for other simulations
+# Experiment 1B (partial reinforcement)
+We simulated a partial reinforcement design, by repeating the CS+ and CS-s in our vectors of training stimuli and setting the trial type to 0 for one of the repeated CS+s. 
+
+# Experimental design
+name <- 'Exp1A' 
+stim_num_d1 <- 11 
+stim_inc_d1 <- 0.25
+stim_num_d2 <- 11 
+stim_inc_d2 <- 0.25 
+dim_lims <- 5
+
+g1_name <- "Single Positive"
+g1_training_stimuli_names <- c("CS+","CS+","CS+","CS+)
+g1_training_trialtypes <- c(1,1,1,0) 
+g1_d1_training_stimuli_elements <- c(6,6,6,6)
+g1_d2_training_stimuli_elements <- c(1,1,1,1)
+g1_N <- 56 
+
+g2_name <- "Double Negative"
+g2_training_stimuli_names <- c("CS1-","CS1-","CS1-","CS1-","CS+","CS+","CS+","CS+","CS2-","CS2-","CS2-","CS2-")
+g2_training_trialtypes <- c(0,0,0,0,1,1,1,0,0,0,0,0) 
+g2_d1_training_stimuli_elements <- c(4,4,4,4,6,6,6,6,8,8,8,8)
+g2_d2_training_stimuli_elements <- c(1,1,1,1,1,1,1,1,1,1,1,1)
+g2_N <- 46 
+
+#test stimuli
+test_stimuli_names <- c("S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","S11")
+d1_test_stimuli_elements <- c(1:11)
+d2_test_stimuli_elements <- c(rep(1,11))
+
+#experiment design
+stimreps_per_block <- 1
+num_blocks <- 3
+
+#saving data
+savetraining <-  TRUE
+savetest <-  TRUE
+
+#Misc.
+digits <-  20
+
+#Experiment 2 (shape-change)
+These inputs are essentially the same as Experiment 1A's, except we simulated the shape-change at test by inputting different elements on d2 between training and test stimuli. The elemental distance between the training and test shape was equal to the elemental distance between GS1 and GS11 on the colour dimension 
+
+#Inputs 
+beta_plus <- 0.2
+beta_minus <- 0.25
+lamb_plus <- 1
+lamb_minus <- 0
+sd <- 1
+
+# Experimental design
+name <- 'Exp1A' 
+stim_num_d1 <- 11 
+stim_inc_d1 <- 0.25
+stim_num_d2 <- 11 
+stim_inc_d2 <- 0.25 
+dim_lims <- 5
+
+g1_name <- "Single Positive"
+g1_training_stimuli_names <- c("CS+")
+g1_training_trialtypes <- c(1) 
+g1_d1_training_stimuli_elements <- c(6)
+g1_d2_training_stimuli_elements <- c(1)
+g1_N <- 46 
+
+g2_name <- "Double Negative"
+g2_training_stimuli_names <- c("CS1-","CS+","CS2-")
+g2_training_trialtypes <- c(0,1,0) 
+g2_d1_training_stimuli_elements <- c(4,6,8)
+g2_d2_training_stimuli_elements <- c(1,1,1)
+g2_N <- 48
+
+#test stimuli
+test_stimuli_names <- c("S1","S2","S3","S4","S5","S6","S7","S8","S9","S10","S11")
+d1_test_stimuli_elements <- c(1:11)
+d2_test_stimuli_elements <- c(rep(11,11)) 
+
+#experiment design
+stimreps_per_block <- 2 
+num_blocks <- 6
